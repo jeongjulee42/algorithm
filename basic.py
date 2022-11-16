457cc17a3d8329f47a3a7ecd0e258bf4d898c4a3
jeongjulee <ljj3342@naver.com>
2022-11-16 19:15:22 +0900

    Feat: basic

---
+++ basic.py
@@ -0,0 +1,17 @@
+a = 1e9
+print(a)
+
+a = 0.3 + 0.6
+print(a)
+print(round(a,4))
+
+a = [0] * 10
+print(a)
+
+array= [i for i in range(20) if i%2 == 1]
+print(array)
+
+n = 3
+m = 4
+array = [[0] * m for _ in range(n)]
+print(array)
