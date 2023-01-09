7934d15c1f9b61742f68888acc3809c4807edc06
jeongjulee <ljj3342@naver.com>
2023-01-09 18:53:25 +0900

    Feat:num3

---
+++ 프로그래머스/코테입문/num3.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = 0;
+    
+    for(let i = 1; i <= n; i++){
+        answer ++;
+        while(answer%3 === 0 || [...String(answer)].includes('3')){
+              answer ++;
+              }
+    }
+    
+    return answer;
+}
+
