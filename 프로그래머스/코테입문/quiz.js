afa49a050fd4a8187bc2305301a8bc027e8d1b19
jeongjulee <ljj3342@naver.com>
2023-03-02 20:56:47 +0900

    Feat:quiz

---
+++ 프로그래머스/코테입문/quiz.js
@@ -0,0 +1,31 @@
+function solution(quiz) {
+    const answer = [];
+    for(item of quiz){
+        let res = myfunc(item)
+        answer.push(res);
+    }
+    return answer;
+}
+
+const myfunc = (express) => {
+    let ary = express.split(' ');
+    ary = ary.map(item => Number.isInteger(parseInt(item)) ? parseInt(item) : item);
+    console.log(ary);
+    if(ary[1] === '+'){
+        let res = ary[0] + ary[2] ;
+        if(res === ary[4]){
+            return "O"
+        }
+        else{
+            return "X"
+        }
+    } else {
+        let res = ary[0] - ary[2] ;
+        if(res === ary[4]){
+            return "O"
+        }
+        else{
+            return "X"
+        }
+    }
+}
