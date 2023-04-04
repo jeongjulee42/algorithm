054a2ef04311934f4b296fdaf83c749873183615
jeongjulee <ljj3342@naver.com>
2023-04-04 21:50:59 +0900

    Feat:count

---
+++ 프로그래머스/코테입문/count.js
@@ -0,0 +1,22 @@
+function solution(score) {
+    let ary = score.map(item => (item[0] + item[1]) / 2)
+    let res = Array.from(ary);
+
+    res = res.map(item => [item, -1]);
+
+    ary.sort((a,b) => b - a);
+    
+    ary = ary.map((item,index) => [item, index + 1])
+    
+    for(let i = 1; i<ary.length; i++){
+        if(ary[i][0] === ary[i-1][0]){
+            ary[i][1] = ary[i-1][1];
+        }
+    }
+
+    for(item of ary){
+        res = res.map(i => i[0] === item[0] ? [i[0],item[1]] : i);
+        console.log(res)
+    }
+    return res.map(item => item[1]);
+}
