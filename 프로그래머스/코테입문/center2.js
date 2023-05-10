80decc8279a91c08958787a74442d47a6b0e4991
jeongjulee <ljj3342@naver.com>
2023-05-10 21:51:15 +0900

    Feat:centerg

---
+++ 프로그래머스/코테입문/center2.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let ary = [...s];
+    if(s.length % 2 === 0){
+        return s[s.length / 2 - 1] + s[s.length / 2]
+    }
+    else {
+        return s[Math.floor(s.length / 2)]
+    }
+}
