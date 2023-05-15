59111ce738b3d01199330a8cc97ef0961dcfa700
jeongjulee <ljj3342@naver.com>
2023-05-15 21:47:57 +0900

    Feat:strstr

---
+++ 프로그래머스/코테입문/strstr.js
@@ -0,0 +1,14 @@
+function solution(str1, str2) {
+    let num = 0
+    for (const item of [...str1]) {
+        if (str2[num] === item) {
+            num++;
+
+            if (num === str2.length) {
+                return 1;
+            }
+            continue;
+        }
+    }
+    return 2;
+}
