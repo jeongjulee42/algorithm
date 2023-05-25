ea66de6c74620e6dec0dbbb8c3bddadf72efc6f7
jeongjulee <ljj3342@naver.com>
2023-05-25 22:50:43 +0900

    Feat:anlge

---
+++ 프로그래머스/코테입문/anlge.js
@@ -0,0 +1,14 @@
+function solution(angle) {
+    if (angle === 180) {
+        return 4;
+    }
+    else if (angle > 90) {
+        return 3;
+    }
+    else if (angle === 90) {
+        return 2;
+    }
+    else {
+        return 1;
+    }
+}
