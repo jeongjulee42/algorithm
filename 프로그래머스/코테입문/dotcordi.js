6dd5a0636b34282a28bcf1fdd4601375e11c4432
jeongjulee <ljj3342@naver.com>
2023-05-30 21:14:50 +0900

    Feat:dotcordi

---
+++ 프로그래머스/코테입문/dotcordi.js
@@ -0,0 +1,14 @@
+function solution(dot) {
+    if(dot[0] > 0){
+        if(dot[1]> 0){
+            return 1;
+        }
+        return 4;
+    }
+    else{
+        if(dot[1]>0){
+            return 2;
+        }
+        return 3;
+    }
+}
