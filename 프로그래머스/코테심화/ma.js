ba0f7793fed4f85262b35cb4e1b9c9da3212c63a
jeongjulee <ljj3342@naver.com>
2024-01-28 20:23:55 +0900

    Feat:ma

---
+++ 프로그래머스/코테심화/ma.js
@@ -0,0 +1,10 @@
+function solution(babbling) {
+    let voca = ['aya', 'ye', 'woo', 'ma'];
+    let ary = Array.from(babbling);
+    for(i of voca){
+        ary = ary.map(item => item.includes(i) ? item.replace(i,'1') : item)
+    }
+    const res = ary.filter(item => Number.isInteger(Number(item)))
+    return res.length;
+}
+
